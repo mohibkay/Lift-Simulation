01f6b3dd6af7750b48680b4801fea38be57eef0b
mohibkay <mohibk.in@gmail.com>
2023-06-05 17:30:40 +0530

    added user input based lifts

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,34 @@
 const liftCar = document.querySelector('.lift-car');
-const upButton = document.querySelector('.up-btn');
+const liftUpBtn = document.querySelector('.liftUpBtn');
+const liftDownBtn = document.querySelector('.liftDownBtn');
 const floorSectionContainer = document.querySelector('.main');
 const floorSection = document.querySelector('.floor-section');
 const formDialog = document.querySelector('.formDialog');
 const inputForm = document.querySelector('.inputForm');
 const dialogBtn = document.querySelector('.dialogBtn');
 const showDialog = document.querySelector('.showDialog');
-const numberOfFloors = document.querySelector('#floors').value;
-const numberOfLifts = document.querySelector('#lifts').value;
-
-formDialog.showModal();
-
-showDialog.addEventListener('click', () => {
-  floorSectionContainer.style.display = 'none';
-  formDialog.showModal();
-});
-
-dialogBtn.addEventListener('click', () => {
-  floorSectionContainer.style.display = 'block';
-  renderFloorSection();
-});
 
 function renderFloorSection() {
+  const numberOfFloors = document.querySelector('#floors').value;
+  const numberOfLifts = document.querySelector('#lifts').value;
   floorSectionContainer.innerHTML = '';
   for (let i = 0; i < numberOfFloors; i++) {
     const floorSectionCloned = floorSection.cloneNode(true);
     const floorCount = floorSectionCloned.querySelector('.floor-number');
-    const elevatorCar = floorSectionCloned.querySelector('.lift-car ');
+    const liftContainer = floorSectionCloned.querySelector('.lift-container');
+    const liftUpBtn = floorSectionCloned.querySelector('.liftUpBtn');
+    const liftDownBtn = floorSectionCloned.querySelector('.liftDownBtn');
+    liftUpBtn.id = `liftUpBtn-${i}`;
+    liftDownBtn.id = `liftDownBtn-${i}`;
 
     if (i === 0) {
-      elevatorCar.style.visibility = 'visible';
+      for (let j = 0; j < numberOfLifts; j++) {
+        const liftCar = document.createElement('div');
+        liftCar.className = 'lift-car';
+        liftCar.id = `lift-${j}`;
+        liftContainer.append(liftCar);
+        liftContainer.append(liftCar);
+      }
     }
     floorCount.textContent = `Floor ${i}`;
     floorSectionContainer.insertBefore(
@@ -45,6 +44,16 @@ const handleUpBtnClick = () => {
   liftCar.classList.add('move');
 };
 
-// renderFloorSection()
+formDialog.showModal();
 
-upButton.addEventListener('click', handleUpBtnClick);
+showDialog.addEventListener('click', () => {
+  floorSectionContainer.style.display = 'none';
+  formDialog.showModal();
+});
+
+dialogBtn.addEventListener('click', () => {
+  floorSectionContainer.style.display = 'block';
+  renderFloorSection();
+});
+console.log('liftUpBtn', liftUpBtn);
+liftUpBtn.addEventListener('click', handleUpBtnClick);
